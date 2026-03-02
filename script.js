b1a8779f1d6979b162c7789ad4ef3f78aaf445ea
NikJur <nikolai.juraschko@rfi.ac.uk>
2026-03-02 17:04:28 +0000

    refactor location data for improved label positioning and visibility

--- script.js
+++ script.js
@@ -22,23 +22,23 @@ window.addEventListener('DOMContentLoaded', () => {
     // Set up location data
     const locations = [
       // --- Education ---
-      { id: 'oxford-dphil', lat: 51.7520, lng: -1.2577, label: 'Oxford University', customLabel: "OU", textOffset: -1.5 /** Nudge text west */, alt: 0.5 /** custom zoom in on globe */ },
-      { id: 'ucl-msci', lat: 51.5246, lng: -0.1340, label: 'UCL', customLabel: "UCL", textOffset: 1.5 /** Nudge text east */ , alt: 2 /** default */ },
-      { id: 'regensburg-abitur', lat: 49.0134, lng: 12.1016, label: 'AMG', customLabel: "AMG", textOffset: 0, alt: 2 },
+      { id: 'oxford-dphil', lat: 51.7520, lng: -1.2577, label: 'Oxford University', customLabel: "Oxford", textOffset: -3.8 /** Nudge text west */, latOffset: 1.1, alt: 0.5 /** custom zoom in on globe */ },
+      { id: 'ucl-msci', lat: 51.5246, lng: -0.1340, label: 'UCL', customLabel: "UCL", textOffset: 1.5 /** Nudge text east */, latOffset: 0 , alt: 2 /** default */ },
+      { id: 'regensburg-abitur', lat: 49.0134, lng: 12.1016, label: 'AMG', customLabel: "AMG", textOffset: 0.2, latOffset: 0, alt: 2 },
 
       // --- Relevant Experience ---
-      { id: 'RSE-AI4Science', lat: 51.7520, lng: -1.2577, label:"", customLabel: "", textOffset: 0, zoomAlt: 0.6 }, // Oxford
-      { id: 'rosalind-franklin', lat: 51.5732, lng: -1.3149, label:"", customLabel: "RFI", textOffset: -1.2, zoomAlt: 0.7 }, // Harwell Campus
-      { id: 'turing-enrichment', lat: 51.5299, lng: -0.1277, label:"", customLabel: "Turing", textOffset: 2.5, zoomAlt: 0.8 }, // London
-      { id: 'turing-dsg', lat: 51.5299, lng: -0.1277, label:"", customLabel: "", textOffset: 0, zoomAlt: 1.2 }, // London (Wider Zoom)
-      { id: 'cambridge-rep', lat: 52.2053, lng: 0.1218, label:"", customLabel: "Cambridge", textOffset: 1.5, zoomAlt: 0.8 }, // Cambridge
-      { id: 'zerog-internship', lat: 50.1109, lng: 8.6821, label:"", customLabel: "zeroG", textOffset: 1.0, zoomAlt: 1.5 }, // Frankfurt
+      { id: 'RSE-AI4Science', lat: 51.7520, lng: -1.2577, label:"", customLabel: "", textOffset: 0, latOffset: 0, zoomAlt: 0.6 }, // Oxford
+      { id: 'rosalind-franklin', lat: 51.5732, lng: -1.3149, label:"", customLabel: "RFI", textOffset: -1.2, latOffset: 0, zoomAlt: 0.7 }, // Harwell Campus
+      { id: 'turing-enrichment', lat: 51.5299, lng: -0.1277, label:"", customLabel: "Turing", textOffset: 3.6, latOffset: 1.1, zoomAlt: 0.8 }, // London
+      { id: 'turing-dsg', lat: 51.5299, lng: -0.1277, label:"", customLabel: "", textOffset: 0, latOffset: 0, zoomAlt: 1.2 }, // London (Wider Zoom)
+      { id: 'cambridge-rep', lat: 52.2053, lng: 0.1218, label:"", customLabel: "Cambridge", textOffset: 1.5, latOffset: 1.8, zoomAlt: 0.8 }, // Cambridge
+      { id: 'zerog-internship', lat: 50.1109, lng: 8.6821, label:"", customLabel: "zeroG", textOffset: -0.1, latOffset: 0, zoomAlt: 1.5 }, // Frankfurt
 
       // --- Other ---
-      { id: 'oxford-tutor', lat: 51.7520, lng: -1.2577, customLabel: "", textOffset: 0, zoomAlt: 0.5 }, // Oxford
-      { id: 'oxford-coach', lat: 51.7520, lng: -1.2577, customLabel: "", textOffset: 0, zoomAlt: 0.6 }, // Oxford
-      { id: 'oxford-editor', lat: 51.7520, lng: -1.2577, customLabel: "", textOffset: 0, zoomAlt: 0.5 }, // Oxford
-      { id: 'ucl-rep', lat: 51.5246, lng: -0.1340, customLabel: "", textOffset: 0, zoomAlt: 1.5 }, // London
+      { id: 'oxford-tutor', lat: 51.7520, lng: -1.2577, customLabel: "", textOffset: 0, latOffset: 0, zoomAlt: 0.5 }, // Oxford
+      { id: 'oxford-coach', lat: 51.7520, lng: -1.2577, customLabel: "", textOffset: 0, latOffset: 0, zoomAlt: 0.6 }, // Oxford
+      { id: 'oxford-editor', lat: 51.7520, lng: -1.2577, customLabel: "", textOffset: 0, latOffset: 0, zoomAlt: 0.5 }, // Oxford
+      { id: 'ucl-rep', lat: 51.5246, lng: -0.1340, customLabel: "", textOffset: 0, latOffset: 0, zoomAlt: 1.5 }, // London
     ];
 
     const updateGlobeRotation = (lat, lng, alt = 2) => {
@@ -87,7 +87,7 @@ window.addEventListener('DOMContentLoaded', () => {
      * at the specified lat/lng coordinates.
      */
     globe.labelsData(locations)
-      .labelLat(d => d.lat)
+      .labelLat(d => d.lat + (d.latOffset || 0))// Applies the nudge North-South based on the latOffset property so labels don't overlap with each other    
       .labelLng(d => d.lng + (d.textOffset || 0)) // Applies the nudge west/east based on the textOffset property so labels don't overlap with each other      
       .labelText(d => d.customLabel)
       .labelSize(1.0) // size of text label
